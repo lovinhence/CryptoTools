802d90e7d00fe05e777288152abacfb42ab4aed0
lovinhence <lovinhence@gmail.com>
2017-10-26 11:57:03 +0800

    api

--- api.py
+++ api.py
@@ -1,9 +1,13 @@
 import requests
 
+# if work,all network requests must go through proxies
+DEBUG = True
+
 
 class Api():
     _exchange_name = ""
 
+    # currency_pair对不同的交易所统一做处理，交易币在前，基准币在后
     def buy(self, currency_pair):
         pass
 
@@ -24,11 +28,12 @@ class HttpApi(Api):
     _headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
     }
-
-    _proxies = {
-        'http': '127.0.0.1:1080',
-        'https': '127.0.0.1:1080'
-    }
+    _proxies = {}
+    if DEBUG:
+        _proxies = {
+            'http': '127.0.0.1:1080',
+            'https': '127.0.0.1:1080'
+        }
 
     def _get(self, url, **kwargs):
         return requests.get(url, headers=self._headers, **kwargs)
@@ -37,13 +42,29 @@ class HttpApi(Api):
         return requests.get(url, headers=self._headers, proxies=self._proxies, **kwargs)
 
 
-def WebSocketApi(Api):
+class WebSocketApi(Api):
     pass
 
 
 class CryptopiaApi(HttpApi):
     __exchange_name = 'cryptopia'
 
-    def getDepth(self, currency_pair):
-        pass
+    def getCurrentPrice(self, currency_pair, direction):
+        if 'buy' == direction:
+            direction = 'Sell'
+        elif 'sell' == direction:
+            direction = 'Buy'
+        result = self._get_proxied("https://www.cryptopia.co.nz/api" + "/GetMarketOrders" + "/" + currency_pair)
+        result_json = result.json()
+        price = result_json['Data'][direction][0]['Price']
+        return float(price)
+
+
+# class GateioApi(HttpApi):
+
+
+if __name__ == '__main__':
+    cryptopiaApi = CryptopiaApi()
+    print(cryptopiaApi.getCurrentPrice("BTM_BTC", "sell"))
+
 
