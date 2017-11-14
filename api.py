046c1cc0a2f0706cdb03ca407141604db772b183
lovinhence <lovinhence@gmail.com>
2017-11-14 11:00:33 +0800

    api

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ DEBUG = True
 
 class Api():
     _exchange_name = ""
+    _baseurl = ''
 
     # currency_pair对不同的交易所统一做处理，交易币在前，基准币在后
     def buy(self, currency_pair):
@@ -31,15 +32,15 @@ class HttpApi(Api):
     _proxies = {}
     if DEBUG:
         _proxies = {
-            'http': '127.0.0.1:1080',
-            'https': '127.0.0.1:1080'
+            'http': '127.0.0.1:1081',
+            'https': '127.0.0.1:1081'
         }
 
     def _get(self, url, **kwargs):
         return requests.get(url, headers=self._headers, **kwargs)
 
-    def _get_proxied(self, url, **kwargs):
-        return requests.get(url, headers=self._headers, proxies=self._proxies, **kwargs)
+    def _get_proxied(self, url, *args, **kwargs):
+        return requests.get(url, headers=self._headers, proxies=self._proxies, *args, **kwargs)
 
 
 class WebSocketApi(Api):
@@ -47,7 +48,7 @@ class WebSocketApi(Api):
 
 
 class CryptopiaApi(HttpApi):
-    __exchange_name = 'cryptopia'
+    _exchange_name = 'cryptopia'
 
     def getCurrentPrice(self, currency_pair, direction):
         if 'buy' == direction:
@@ -60,11 +61,73 @@ class CryptopiaApi(HttpApi):
         return float(price)
 
 
-# class GateioApi(HttpApi):
+class GateioApi(HttpApi):
+    _exchange_name = 'gateio'
+    _baseurl = 'http://data.gate.io/api2/1'
+
+    def getCurrentPrice(self, currency_pair, direction):
+        if 'buy' == direction:
+            direction = 'asks'
+        elif 'sell' == direction:
+            direction = 'bids'
+        result = self._get_proxied(self._baseurl + "/orderBook/" + currency_pair)
+        result_json = result.json()
+        price = result_json[direction][0][0]
+        return float(price)
+
+
+class HitbtcWebSocketApi(WebSocketApi):
+    pass
+
+
+class HitbtcApi(HttpApi):
+    _exchange_name = 'hitbtc'
+
+    def getCurrentPrice(self, currency_pair, direction):
+        if 'buy' == direction:
+            direction = 'asks'
+        elif 'sell' == direction:
+            direction = 'bids'
+        result = self._get_proxied("http://api.hitbtc.com/api" + "/1/public/" + currency_pair + "/orderbook")
+        result_json = result.json()
+        price = result_json[direction][0][0]
+        return float(price)
+
+
+class BitfinexApi(HttpApi):
+    _exchange_name = 'bitfinex'
+
+
+class BittrexApi(HttpApi):
+    _exchange_name = 'bittrex'
+
+
+class BinanceApi(HttpApi):
+    _exchange_name = 'binance'
+    _baseurl = "https://www.binance.com/api"
+
+    def getCurrentPrice(self, currency_pair, direction):
+        if 'buy' == direction:
+            direction = 'asks'
+        elif 'sell' == direction:
+            direction = 'bids'
+        params = {
+            "symbol": currency_pair
+        }
+        result = self._get_proxied(self._baseurl + "/v1/depth", params)
+        result_json = result.json()
+        price = result_json[direction][0][0]
+        return float(price)
 
 
 if __name__ == '__main__':
-    cryptopiaApi = CryptopiaApi()
-    print(cryptopiaApi.getCurrentPrice("BTM_BTC", "sell"))
+    # cryptopiaApi = CryptopiaApi()
+    # print(cryptopiaApi.getCurrentPrice("BTM_BTC", "sell"))
+    # hitbtcApi = HitbtcApi()
+    # print(hitbtcApi.getCurrentPrice("BTMETH", "sell"))
+    gateioApi = GateioApi()
+    print(gateioApi.getCurrentPrice("btm_eth", "buy"))
+    # binanceApi = BinanceApi()
+    # print(binanceApi.getCurrentPrice("LRCETH", "buy"))
 
 
